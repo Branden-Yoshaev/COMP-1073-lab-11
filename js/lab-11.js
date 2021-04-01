991e999517e7dab7603f866b9e6b4b972b4d4fd6
Branden-Yoshaev <bsyoshae@lakeheadu.ca>
2021-04-01 12:43:11 -0400

    Adding comments to js

--- js/lab-11.js
+++ js/lab-11.js
@@ -1,24 +1,30 @@
+   //Const for my variables
     const list = document.querySelector('ul');  
     const input = document.querySelector('input');
     const button = document.querySelector('button');
 
+    //Creating onClick function for the button
     button.onclick = function() {
     let myItem = input.value;
     input.value = '';
 
+    //Creating DOM elements
     const listItem = document.createElement('li');
     const listText = document.createElement('span');
     const listBtn = document.createElement('button');
 
     listItem.appendChild(listText);
     listText.textContent = myItem;
+
+    //Creating Onclick for delete button
+    listBtn.onclick = function() {
+        list.removeChild(listItem);
+    }
+
     listItem.appendChild(listBtn);
     listBtn.textContent = 'Delete';
     list.appendChild(listItem);
 
-    listBtn.onclick = function() {
-    list.removeChild(listItem);
-    }
-
+    //focus method in order for the input element
     input.focus();
     }
